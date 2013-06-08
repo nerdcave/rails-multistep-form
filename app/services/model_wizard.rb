22a71255afbe79a151704d1057932f19d45c453d
Jay Elaraj <jay@nerdcave.com>
2013-06-08 12:03:34 -0400

    Abstracted session params name

--- app/services/model_wizard.rb
+++ app/services/model_wizard.rb
@@ -5,22 +5,23 @@ class ModelWizard
     @object_class = object_class
     @session = session
     @params = params
+    @session_params = "#{object_class.name.underscore}_params".to_sym
   end
 
   def start(object = nil)
-    @session[:object_params] = {}
-    @object = object || @object_class.new(@session[:object_params])
+    @session[@session_params] = {}
+    @object = object || @object_class.new(@session[@session_params])
     @object.current_step = 0
     self
   end
 
   def process(param_name, object = nil)
-    @session[:object_params].deep_merge!(@params[param_name]) if @params[param_name]
+    @session[@session_params].deep_merge!(@params[param_name]) if @params[param_name]
     if object.nil?
-      @object = @object_class.new(@session[:object_params])
+      @object = @object_class.new(@session[@session_params])
     else
       @object = object
-      @object.assign_attributes(@session[:object_params])
+      @object.assign_attributes(@session[@session_params])
     end
     self
   end
@@ -33,7 +34,7 @@ class ModelWizard
       if @object.last_step?
         if @object.all_steps_valid?
           saved = @object.save
-          @session[:object_params] = nil
+          @session[@session_param] = nil
         end
       else
         @object.step_forward
